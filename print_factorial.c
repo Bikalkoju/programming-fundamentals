937767f4fd39f50e5a5dce068cbf7103f02238d5
Bikalkoju <bikalkoju123@gmail.com>
2016-10-26 12:10:07 +0545

    Add files via upload
    Write a programme to print factorial of any no. provided by user using while loop

---
+++ print_factorial.c
@@ -0,0 +1,27 @@
+/*
+Name:Bikal Koju
+Subject:Programming fundamentals
+Roll :
+Lab no:09
+Program:Write a programme to print factorial of any no. provided by user using while loop
+Date:2016-10-26
+*/
+
+#include<stdio.h>
+
+int main()
+{
+	int n,fact=1;
+		printf("Enter a number:");
+		scanf("%d",&n);
+		
+	while (n !=0)
+	{
+		fact *=n;
+		--n;
+	}
+		
+		printf("\nThe factorial of no. is:%d",fact);
+	
+return 0;	
+}
