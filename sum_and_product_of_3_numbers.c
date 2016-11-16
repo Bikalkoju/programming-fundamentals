1e317b4367d588c29911a00804fac37a69c71725
Bikalkoju <bikalkoju123@gmail.com>
2016-11-16 12:53:26 +0545

    Update sum_and_product_of_3_numbers.c

--- sum_and_product_of_3_numbers.c
+++ sum_and_product_of_3_numbers.c
@@ -12,22 +12,22 @@ Date:2016-11-16
 
 int main()
 {
-    int a,b,c,sum,mul;
-        printf("\t000000000000000000000000000000000000000000000000000000\n");
-        printf("\n\t\tEnter First Number:");
+   int a,b,c,sum,mul;
+        printf("000000000000000000000000000000000000000000000000000000\n");
+        printf("\n\tEnter First Number:");
         scanf("%d",&a);
         printf("\n\t\tEnter Second Number:");
         scanf("%d",&b);
-        printf("\n\t\tEnter third Number:");
+        printf("\n\t\t\tEnter Third Number:");
         scanf("%d",&c);
-        printf("\n\t0000000000000000000000000000000000000000000000000000000\n");
+        printf("\n\t\t\t0000000000000000000000000000000000000000000000000000000\n");
 
         sum=a+b+c;
         mul=a*b*c;
 
-        printf("\n\t\t=> Sum of three number are %d",sum);
-        printf("\n\n\t\t=> product of three number are %d",mul);
-        printf("\n\n\t0000000000000000000000000000000000000000000000000000000\n");
+        printf("\n\t\t\t\t=> Sum of three number are %d",sum);
+        printf("\n\n\t\t\t\t\t=> product of three number are %d",mul);
+        printf("\n\n\t\t\t\t\t0000000000000000000000000000000000000000\n");
 
     getch();
 return 0;
