8616d51748ebd2e07974507b63d18158c5a17bb7
Bikalkoju <bikalkoju123@gmail.com>
2016-11-23 10:39:57 +0545

    Add files via upload

---
+++ convert_centigrade_into_fahrenheit.c
@@ -0,0 +1,24 @@
+/*
+Student Name : Bikal Koju
+Roll No. : 03
+Subject : Programming Fundamental
+Lab No. : 05
+Program : Write a c program to 
+Date : 23 nov, 2016
+*/
+
+#include<stdio.h>
+
+int main ()
+{
+	float f,c;
+		printf("\nInput temperature in centigrade:");
+		scanf("%f",&c);
+		
+		f=((9*c)/5)+32;
+		
+		printf("\n\nTemperature in fahrenheit is %f",f);
+	
+
+	return 0;
+}
