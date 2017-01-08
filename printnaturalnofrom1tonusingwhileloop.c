aa93c19f51dd3eb5f754f0ce7a7ce1f2786b42fc
Bikalkoju <bikalkoju123@gmail.com>
2017-01-08 13:01:24 +0545

    Add files via upload

---
+++ printnaturalnofrom1tonusingwhileloop.c
@@ -0,0 +1,22 @@
+/*
+Name    : Bikal Koju
+Roll no :
+subject : Programming fundamentals
+Lab no  : 16
+Program : WAP to print allnatural no from 1 to n using while loop 
+Date    : 8 jan 2017
+*/
+
+#include<stdio.h>
+
+int main()
+{
+	int n=1;
+	
+	while(n<=100)
+	{
+		printf("\t%d",n);
+		n++;
+	}
+return 0;
+}
